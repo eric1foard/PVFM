2f0be4ad0aac31c3e550eacde0d3a4b32101750e
Eric Foard <foard265@gmail.com>
2015-05-31 10:13:52 -0700

    finish vendor show modals

--- farmers_market/src/AppBundle/Resources/public/js/pvfm.js
+++ farmers_market/src/AppBundle/Resources/public/js/pvfm.js
@@ -1,32 +1,52 @@
-var app = angular.module('PVFM', ["ui.bootstrap.modal"]);
+var app = angular.module('PVFM', ['ui.bootstrap']);
 
-app.controller('userCtrl', ['$scope', '$http',
-	function($scope, $http) {
+app.controller('userCtrl', ['$scope', '$http', '$modal',
+	function($scope, $http, $modal) {
 
-		$scope.test = "hello";
 		var users = [];
-
 		$http.get('user/getAllIDs').success(function(data){
-				
-				angular.forEach(data, function(value, key){
-					users.push(value);
-				});
-				$scope.users = users;
-				});
+			angular.forEach(data, function(value, key){
+				users.push(value);
+			});
+			$scope.users = users;
+		});
+
 
 		$scope.getUserInfo = function(id){
+			$scope.animationsEnabled = true;
 			$http.get('user/getUserInfo/'+id).success(function(data){
 
 				$scope.userInfo = data;
 
-				console.log("name: "+ $scope.userInfo.username);
-				$scope.showModal = true;
-
-
+				var modalInstance = $modal.open({
+					animation: true,
+					templateUrl: 'modal.html',
+					controller: 'ModalInstanceCtrl',
+					size: 'lg',
+					resolve: {
+						userInfo: function () {
+							return $scope.userInfo;
+						}
+					}
 				});
+
+				
+			});
 		};
-
-		
-
 	}
 	]);
+
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+
+app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, userInfo) {
+
+	$scope.userInfo = userInfo;
+
+	console.log("from the ModalInstanceCtrl: "+ userInfo.username);
+
+	$scope.cancel = function () {
+		$modalInstance.dismiss('cancel');
+	};
+});
